e80ae89eda3a866a26ecafba9b9115f60c3fa340
LoraKlets <lloraklets@gmail.com>
2017-01-26 00:25:29 +0900

    исправлены 2 замечания по пробелам

--- js/stat.js
+++ js/stat.js
@@ -31,8 +31,8 @@ function renderStatistics(ctx, names, times) {
     if (name === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-        ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random().toFixed(1) + ')';
-      }
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random().toFixed(1) + ')';
+    }
     ctx.fillRect(histoX + columnIndent * i, bottomY - height, 40, height);
     ctx.fillStyle = '#000';
     ctx.fillText(time.toFixed(0), histoX + columnIndent * i + 8, bottomY - height - 6);
