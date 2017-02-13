0d0cec99b433c1ad3065c6df3c9c90d8360252fb
LoraKlets <lloraklets@gmail.com>
2017-02-13 14:04:35 +0900

    travis correct

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,30 +1,13 @@
 'use strict';
-window.colorizeElement = (function(element, colors, property){
-  var currentColor = element.style[property];
-  element.addEventListener('click', function () {
-  element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
-    });
-
-  element.addEventListener('keydown', function (evt) {
-    if (window.utils.isActivateEvent(evt)) {
-      element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
-    };
-    });
-});
-/*function (element, colors, property) {
+window.colorizeElement = (function (element, colors, property) {
   var currentColor = element.style[property];
   element.addEventListener('click', function () {
     element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
   });
-  var ENTER_KEY_CODE = 13;
-  var isActivateEvent = function (evt) {
-    return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-  };
 
   element.addEventListener('keydown', function (evt) {
-    if (isActivateEvent(evt)) {
+    if (window.utils.isActivateEvent(evt)) {
       element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
     }
   });
-};
-*/
+});
--- js/util.js
+++ js/util.js
@@ -1,34 +1,21 @@
 'use strict';
-window.utils = (function(){
+window.utils = (function () {
   var ENTER_KEY_CODE = 13;
   var getRandomElement = function (array) {
-  var randomElementIndex = Math.floor(Math.random() * array.length);
+    var randomElementIndex = Math.floor(Math.random() * array.length);
     return array[randomElementIndex];
   };
   return {
-    getRandomElement : getRandomElement,
-    getRandomElementExcept : function (array, currentElement) {
+    getRandomElement: getRandomElement,
+    getRandomElementExcept: function (array, currentElement) {
       var newColor;
       while (!newColor || newColor === currentElement) {
         newColor = window.utils.getRandomElement(array);
-      };
+      }
       return newColor;
     },
-    isActivateEvent : function (evt) {
+    isActivateEvent: function (evt) {
       return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
     }
   };
 })();
-/*{
-  getRandomElement: function (array) {
-    var randomElementIndex = Math.floor(Math.random() * array.length);
-    return array[randomElementIndex];
-  },
-  getRandomElementExcept: function (array, currentElement) {
-    var newColor;
-    while (!newColor || newColor === currentElement) {
-      newColor = window.utils.getRandomElement(array);
-    }
-    return newColor;
-  }
-};*/
